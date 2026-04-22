a8ce84573e19b120b67faa1d444b4b68b15c603d
Jason Shultz <jasshultz@gmail.com>
2026-04-21 18:33:26 -0700

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -35,9 +35,9 @@ export default withSentryConfig(nextConfig, {
 	// For all available options, see:
 	// https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-	org: 'volunteer-ready-org',
+	org: 'volunteerready',
 
-	project: 'javascript-nextjs',
+	project: 'volunteer-ready-org',
 
 	// Only print logs for uploading source maps in CI
 	silent: !process.env.CI,
